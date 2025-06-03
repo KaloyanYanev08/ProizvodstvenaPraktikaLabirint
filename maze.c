30911ca9de4239e8a2f1799b0655f9bd4935cb13
Vnzmiau <viliyan.balchev@gmail.com>
2025-06-03 17:46:39 +0300

    Created maze.c and implemented init2dMaze

---
+++ maze.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int** init2dMaze(int row,int column){
+    int** maze = (int**)malloc(row * sizeof(int*));
+    for(int i = 0; i < row; i++){
+        maze[i] = (int*)malloc(column * sizeof(int));
+    }
+    for(int i = 0; i < row; i++){
+        for(int j = 0; j < column; j++){
+            maze[i][j] = 1; // Initialize all cells to walls
+        } //0 is empty, 1 is a wall
+    }
+    return maze;
+
+}
+
+int main(){
+    
+}
